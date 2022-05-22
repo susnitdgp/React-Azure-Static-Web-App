9a6dfdb0e6dbb2baffe2e9e5e88ef699b9381411
Susanta Goswami <susnitdgp@gmail.com>
2022-05-22 10:59:08 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react'; 
+
 
 function App() {
+  
+  //Usage of state
+  const [total, setTotal] = useState(120);
+
   const value = 'Goswami World';
-  return <div>Hello {value}</div>;
+  
+  return (
+    <div>
+
+    <div>Hello:  {value}</div>
+    <div>Count: {total}</div>
+
+    </div>
+
+  )
 }
 
 export default App;
